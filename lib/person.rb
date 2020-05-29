f241b739fb189e225f2be1b2e7a21ae9ec2389f6
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-05-29 16:23:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,11 @@
 class Person
   def initialize(name)
-  @name = name
-end
+    @name = name
+  end
   def name=(name)
     @name = name
-def name
-  @name
-end
+  end
+  def name
+    @name
+  end
 end
