73246d0b5487bccc11d2d7e9acab72e1924e3278
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-05-29 16:26:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,24 @@
 class Dog
-def initiailize(name, breed)
-  @name = name
-end
+
+  def initiailize(name, breed)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
 
 end
